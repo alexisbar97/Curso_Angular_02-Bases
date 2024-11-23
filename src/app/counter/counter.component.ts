620bc262da238dffdd04ec78f8f1a957c6d231b1
Alexis Andrés Barría Cabello <alexis.barria.c@outlook.cl>
2024-11-23 18:49:14 -0300

    3. Video 42 Finalizado

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-counter',
+  template: '<h1>¡Hola Counter!</h1>',
+  standalone: false,
+})
+
+export class CounterComponent {
+
+}
