ab928dc6e88626f0ffb40e50f63cf2ba99aea46c
Alexis Andrés Barría Cabello <alexis.barria.c@outlook.cl>
2024-11-23 21:06:55 -0300

    10. Video 49 Finalizado

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-list',
   standalone: false,
-
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
-export class ListComponent {
 
+export class ListComponent {
+  public heroNames: string[] = ['Spiderman','Ironman','Hulk','She Hulk','Thor'];
 }
