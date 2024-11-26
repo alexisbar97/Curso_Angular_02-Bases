16d0ee0b4026267a3feab636f7d4def8dae2d825
Alexis Andrés Barría Cabello <alexis.barria.c@outlook.cl>
2024-11-26 11:25:34 -0300

    5. Video 63 Finalizado

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,6 +1,7 @@
 // a-component para crear la plantilla del componente
 
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -9,5 +10,17 @@ import { Component } from '@angular/core';
 })
 
 export class MainPageComponent {
-
+  public characters: Character[] = [{
+    name: 'Krillin',
+    power: 1000,
+  },
+  {
+    name: 'Gokú',
+    power: 9500,
+  },
+  {
+    name: 'Vegeta',
+    power: 7500,
+  },
+  ];
 }
