d02747b5b2e9349be4935d6d22007c4433dc0bc4
Alexis Andrés Barría Cabello <alexis.barria.c@outlook.cl>
2024-11-26 15:27:17 -0300

    9. Video 67 Finalizado

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -9,6 +9,9 @@ import { Character } from '../../interfaces/character.interface';
 })
 
 export class AddCharacterComponent {
+  @Output()
+  onNewCharacter: EventEmitter<Character> = new EventEmitter();
+
   public character: Character = {
     name: '',
     power: 0,
@@ -16,5 +19,14 @@ export class AddCharacterComponent {
 
   emitCharacter(): void {
     console.log(this.character);
+
+    if (this.character.name.length === 0) return;
+
+    this.onNewCharacter.emit(this.character);
+
+    this.character.name = '';
+    this.character.power = 0;
   }
+
+
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -23,4 +23,9 @@ export class MainPageComponent {
     power: 7500,
   },
   ];
+
+  onNewCharacter(character: Character): void {
+    console.log('MainPage');
+    console.log(character);
+  }
 }
